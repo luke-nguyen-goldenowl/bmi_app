cdf9646ed9fe1ba0edcd352e11ac0b242e2d234a
Luke Nguyen <luke.nguyen.goldenowl@gmail.com>
2021-11-03 00:52:05 +0700

    check size again

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -22,7 +22,7 @@ class ResultScreen extends StatelessWidget {
           title: Text(
             'YOUR BMI',
             style: TextStyle(
-              fontSize: 30,
+              fontSize: 50,
             ),
           ),
         ),
